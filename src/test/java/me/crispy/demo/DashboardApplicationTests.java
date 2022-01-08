258a60133d9350041dffb200d720a5657dbede2b
Crispy <gunho19945@gmail.com>
2022-01-08 20:19:39 +0900

    [개발] 스켈레톤 코드 작성

---
+++ src/test/java/me/crispy/demo/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.crispy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
